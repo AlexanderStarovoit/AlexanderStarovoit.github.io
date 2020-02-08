c0c4ae9a1eaf04df8bdbe557ed697386a2626ac4
AlexanderStarovoit <58905325+AlexanderStarovoit@users.noreply.github.com>
2020-02-08 11:39:49 +0300

    Revert "Revert "Aded more pictures""
    This reverts commit 541d54ece483a68a3bf7dae8a7c2f2878a4bd50e.

--- js/components/gallery.js
+++ js/components/gallery.js
@@ -10,6 +10,12 @@ const IMAGES = [
 	"img/gallery/7.JPG",
 	"img/gallery/8.JPG",
 	"img/gallery/9.JPG",
+	"img/gallery/10.JPG",
+	"img/gallery/11.JPG",
+	"img/gallery/12.JPG",
+	"img/gallery/13.JPG",
+	"img/gallery/14.JPG",
+	"img/gallery/15.JPG",
 
 
 ];
